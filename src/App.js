e77cd9ad24f168c92185e17f615e69beaa9f2a8c
wildapt01 <pb5191@gmail.com>
2018-09-28 18:05:48 -0400

    added MenuBar.js, start config of MenuBar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './SignIn';
 import Map from './Map';
+import MenuBar from './components/MenuBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <SignIn />
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <SignIn /> */}
+
+          <MenuBar />
         </header>
         <Map />
       </div>
