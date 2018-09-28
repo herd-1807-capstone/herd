7298ffa73ac048915314b93107b22521e23eb419
mina-gh <minamail@gmail.com>
2018-09-28 17:32:48 -0400

    worked on tour apis - get and post

--- server/api/tours.js
+++ server/api/tours.js
@@ -7,15 +7,26 @@ module.exports = router;
 router.get('/:tourId', async(req, res, next) => {
   const tourId = req.params.tourId;
   try{
-    console.log(firebase.auth().currentUser)
+    const user = firebase.auth().currentUser;
 
-    await db.ref(`/tours/${tourId}`).once('value').then(snapshot => {
-      if(!snapshot){
-        res.status(404);
-      }else{
-        res.json(snapshot);
+    // a user must be logged-in to retrieve data.
+    if(!user){
+      res.status(403).send('forbidden');
+      return;
+    }
+    db.ref(`/tours/${tourId}`).once('value').then (snapshot => {
+      const members = snapshot.val().users;
+
+      const memberIds = Object.keys(members);
+      // check if current user is either an admin of this tour or a member.
+      if(memberIds.indexOf(user.id) < 0){
+        res.status(403).send('forbidden');
+        return;
       }
+
+      res.json(snapshot);
     });
+
   }catch(err){
     next(err);
   }
@@ -28,24 +39,40 @@ router.post('/:tourName', async(req, res, next) => {
     const spots = req.body.spots;
     const users = req.body.users;
 
-    console.log(firebase.auth().currentUser)
+    const user = firebase.auth().currentUser;
+    if(!user || user.status.toLowerCase() !== 'admin'){
+      res.status(403).send('forbidden');
+      return;
+    }
 
     await db.ref(`/tours/`).push({
       tourName,
       spots,
       users
     });
+
+    res.status(201).send();
   }catch(err){
     next(err);
   }
 })
 
 // PUT
-router.put('/XXX', async(req, res, next) => {
+router.put('/:tourId', async(req, res, next) => {
+  const tourName = req.params.tourName;
+  const spots = req.body.spots;
+  const users = req.body.users;
+
+  const user = firebase.auth().currentUser;
+  if(user.status.toLowerCase() !== 'admin'){
+    res.status(403).send('forbidden');
+    return;
+  }
+
 
 })
 
 // DELETE
-router.delete('/XXX', async(req, res, next) => {
+router.delete('/:tourId', async(req, res, next) => {
 
 })
