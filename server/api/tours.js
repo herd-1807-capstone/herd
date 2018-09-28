d66c242f9f85adc8f69c779600643da859bd7f98
Peng <pengliao2010@gmail.com>
2018-09-28 14:19:27 -0400

    add tour group

--- server/api/tours.js
+++ server/api/tours.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const db = require('../db')
+const firebase = require('firebase')
 module.exports = router;
 
 // GET
@@ -8,6 +9,7 @@ router.get('/:tourId', async(req, res, next) => {
   try{
     await db.ref(`/tours/${tourId}`).once('value').then(snapshot => {
       res.json(snapshot);
+      console.log(firebase.auth().currentUser)
     });
   }catch(err){
     console.log(err);
@@ -15,8 +17,17 @@ router.get('/:tourId', async(req, res, next) => {
 })
 
 // POST
-router.post('/XXX', async(req, res, next) => {
-
+router.post('/:tourName', async(req, res, next) => {
+  const tourName = req.params.tourName;
+  db.ref(`/tours/`).push({
+    tourName: tourName,
+    spots: {},
+    users: {"4GDfXiHKt1Pf5VbKGuqsR2bU9pl2" : {
+      "email" : "anpoon430@gmail.com",
+      "name" : "andy poon"
+    }
+  },
+  })
 })
 
 // PUT
