644908011bdb924f3f9e6094b2ac5620dc090bd5
wildapt01 <pb5191@gmail.com>
2018-10-01 18:17:24 -0400

    added  component, removed style on Map

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+import ChatView from './ChatView';
+import ChatForm from './ChatForm';
+
+const Chat = () => {
+  return (
+    <div>
+      <ChatView />
+      <ChatForm />
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/components/ChatForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const ChatForm = () => {
+  return (
+    <div>
+      <div>Input of new message</div>
+      <button type="Submit">Send</button>
+    </div>
+  );
+};
+
+export default ChatForm;
