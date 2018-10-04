a39d33f965353e5c29db2a7871c47073eb54dea9
wildapt01 <pb5191@gmail.com>
2018-10-04 19:01:06 -0400

    added ChatView component

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -1,7 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
-const ChatView = () => {
-  return <div>See the message list between 2 users</div>;
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 400,
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 150,
+  },
+  listSection: {
+    backgroundColor: 'inherit',
+  },
+  ul: {
+    backgroundColor: 'inherit',
+    padding: 0,
+  },
+});
+
+function ChatView(props) {
+  const { classes } = props;
+
+  return (
+    <List className={classes.root}>
+      <ul className={classes.ul}>
+        {[0, 1, 2].map(item => (
+          <ListItem key={`item-${sectionId}-${item}`}>
+            <ListItemText primary={`Item ${item}`} />
+          </ListItem>
+        ))}
+      </ul>
+    </List>
+  );
+}
+
+ChatView.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
-export default ChatView;
+export default withStyles(styles)(ChatView);
