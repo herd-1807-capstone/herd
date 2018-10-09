481aa6812e61062d7aa138b228871f367e865f01
Peng <pengliao2010@gmail.com>
2018-10-09 17:12:09 -0400

    temporary

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import firebase from './fire';
+import { connect } from 'react-redux';
+import './App.css';
+import Login from './Login';
+import Error from './Error';
+import MenuBar from './components/MenuBar';
+import PostLogin from './components/PostLogin';
+import { setCurrentUser } from './store/index';
+import Admin from './components/Admin';
+import CreateGroup from './components/CreateGroup';
+import ManageGroup from './components/ManageGroup';
+import LoadingState from './components/LoadingState'
+import { changeLoadingState } from './reducers/user';
+
+const auth = firebase.auth();
+const db = firebase.database();
+
+class App extends Component {
+
+  componentDidMount() {
+    auth.onAuthStateChanged(async user => {
+      if (user) {
+        const connectedRef = db.ref('.info/connected');
+
+        try {
+          let snapshot = await db.ref(`/users/${user.uid}`).once('value');
+          console.log("On Auth current user")
+          console.log(auth.currentUser.emailVerified)
+          console.log("user does exist!")
+          console.log(user)
+          console.log("what we have for the user")
+          console.log(snapshot.val())
+          const userRef = db.ref(`/users/${user.uid}`)
+          if (!snapshot.exists()) {
+            let newUser = {
+              name: user.displayName,
+              email: user.email,
+              phone: user.phoneNumber,
+              uid: user.uid,
+              status: 'member',
+              visible: true,
+              tour: null,
+              loggedIn: true,
+            };
+            await db.ref(`/users/${user.uid}`).set(newUser);
+            newUser = await db.ref(`/users/${user.uid}`).once('value');
+            user = newUser.val();
+
+
+
+          } else {
+            let theUser = {
+              name: user.displayName,
+              email: user.email,
+              phone: user.phoneNumber,
+              imgUrl: user.photoURL,
+              loggedIn: true
+            }
+            await db.ref(`/users/${user.uid}`).update(theUser)
+
+            theUser.uid = user.uid
+            theUser.status = snapshot.val().status
+            theUser.visible = snapshot.val().visible
+            theUser.tour = snapshot.val().tour
+            user = theUser
+          }
+          this.props.setCurrentUser(user)
+          userRef.onDisconnect().update({loggedIn: false});
+
+          connectedRef.on('value', (snap)=>{
+            if(snap.val() === true) userRef.update({loggedIn: true});
+            else userRef.update({loggedIn: false});
+          })
+
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        console.log("User does not exist")
+        this.props.history.push('/signin')
+      }
+    });
+  }
+
+
+  render() {
+    const tour = this.props.currentUser.tour;
+    return (
+      <div className="App">
+
+        <Switch>
+          <Route path="/signin" component={Login} />
+          {this.props.currentUser.hasOwnProperty('email') && (
+            <Switch>
+              {
+                tour ?
+                <Route exact path="/" component={MenuBar} />
+                :
+                <Route exact path="/" component={PostLogin} />
+              }
+              <Route path="/error/:id" component={Error} />
+              <Route exact path="/admin" component={Admin} />
+              <Route exact path="/admin/group" component={ManageGroup} />
+              <Route path="/admin/group/create" component={CreateGroup} />
+            </Switch>
+          )}
+          {<Route component={LoadingState} />}
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    currentUser: state.user.currentUser,
+    isLoading: state.user.isLoading,
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+    changeLoadingState: () => dispatch(changeLoadingState())
+  }
+}
+
+export default withRouter(connect(mapState, mapDispatch)(App));
